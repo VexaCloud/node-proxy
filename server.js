a0faa2acce1a3b8c4fee2f05080f4ae302096199
VexaCloud <orrysander@gmail.com>
2026-04-27 14:52:27 -0700

    Delete server.js

--- server.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-
-const app = express();
-
-app.get("/", (req, res) => res.send("Hello from Node behind nginx!"));
-
-app.listen(3000, () => console.log("Node on 3000"));
